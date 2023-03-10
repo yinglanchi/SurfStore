174340210ea8ea0bd6a16eea8d5817d9edba9672
yinglan chi <elaine@yinglans-MBP.attlocal.net>
2023-03-10 15:35:34 -0800

    fix consistentHashing bug

--- pkg/surfstore/ConsistentHashRing.go
+++ pkg/surfstore/ConsistentHashRing.go
@@ -3,6 +3,7 @@ package surfstore
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"sort"
 )
 
 type ConsistentHashRing struct {
@@ -10,16 +11,23 @@ type ConsistentHashRing struct {
 }
 
 func (c ConsistentHashRing) GetResponsibleServer(blockId string) string {
+	hashes := []string{}
+	for h := range c.ServerMap {
+		hashes = append(hashes, h)
+	}
+	sort.Strings(hashes)
 	blockHash := c.Hash(blockId)
-	for key := range c.ServerMap {
-		if key >= blockHash {
-			return c.ServerMap[key]
+	responsibleServer := ""
+	for i := 0; i < len(hashes); i++ {
+		if hashes[i] > blockHash {
+			responsibleServer = c.ServerMap[hashes[i]]
+			break
 		}
 	}
-	for key := range c.ServerMap {
-		return c.ServerMap[key]
+	if responsibleServer == "" {
+		responsibleServer = c.ServerMap[hashes[0]]
 	}
-	panic("no servers available")
+	return responsibleServer
 
 }
 
